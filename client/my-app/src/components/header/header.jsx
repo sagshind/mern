52bd966511b7b439f2f7f9bf96ceb9f5a5c349da
Sagar Shinde <sagar.shinde@aurusit.com>
2023-05-10 18:39:53 +0530

    added footer

--- client/my-app/src/components/header/header.jsx
+++ client/my-app/src/components/header/header.jsx
@@ -13,6 +13,14 @@ const header = (props) =>{
             <li><Link to="/contact-us">contact-us</Link></li>
         </ul>
         <Outlet/>
+        <ul>
+        <li><Link to="/">Home</Link></li>
+            <li><Link to="/about-us">about-us</Link></li>
+            <li><Link to="/projects">projects</Link></li>
+            <li><Link to="/events">events</Link></li>
+            <li><Link to="/donation">donation</Link></li>
+            <li><Link to="/contact-us">contact-us</Link></li>
+        </ul>
 </div>)
 }
 
